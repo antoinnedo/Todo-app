b168b7881da073a570249c5b8b3b0551acc7c9cb
Anthony Do <anthonydo2307@gmail.com>
2024-11-02 23:09:00 -0500

    nov2nd-2

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,12 +1,12 @@
 export function Tabs(props) {
   const { todos } = props
 
-  const tabs = ['All', 'Open', 'Completed', 'haha']
+  const tabs = ['All', 'Open', 'Completed']
 
   return (
     <nav className="tab-container">
       {tabs.map((tab, tabIndex) => {
-        const numOfTasks = tab === 'All' ? 
+        const numberOfTasks = tab === 'All' ? 
           todos.length :
           tab === 'Open' ?
             todos.filter(val => !val.complete).length : todos.filter(val => val.complete).length
