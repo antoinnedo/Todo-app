59b9c4389c53a3143a2911e9e8ea7a101125d33d
Anthony Do <anthonydo2307@gmail.com>
2024-11-02 23:48:38 -0500

    nov2nd-4

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,6 +1,5 @@
 export function TodoCard(props) {
-  const {todoIndex, todos} = props
-  const todo = todos[todoIndex]
+  const { todo } = props 
   return (
     <div className="card todo-item">
       <p>{todo.input}</p>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,20 +3,22 @@ import { TodoCard } from "./TodoCard";
 export function TodoList(props) {
   const { todos } = props
 
-  const tab = 'All'
+  const tab = 'Completed'
 
   const filterTodosList = tab === 'All' ?
     todos :
     tab === 'Completed' ?
-      todos.filter(val => val.completed) :
-      todos.filter(val => !val.completed)
+      todos.filter(val => val.complete) :
+      todos.filter(val => !val.complete)
 
   return (
     <>
       
-      {todos.map((todo, todoIndex) => {
+      {filterTodosList.map((todo, todoIndex) => {
         return (
-          <TodoCard key={todoIndex} todoIndex={todoIndex} {...props}/>
+          <TodoCard 
+            key={todoIndex} 
+            todo = {todo}/>
         )
       })}
 
